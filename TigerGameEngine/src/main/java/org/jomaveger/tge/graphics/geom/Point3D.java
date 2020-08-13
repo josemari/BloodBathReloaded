4408656afe3d64ff7f1928782b76f7409ccb5833
josemari <jomaveger@gmail.com>
2020-08-13 14:02:47 +0200

    updates

---
+++ TigerGameEngine/src/main/java/org/jomaveger/tge/graphics/geom/Point3D.java
@@ -0,0 +1,44 @@
+package org.jomaveger.tge.graphics.geom;
+
+public class Point3D {
+
+	private float x;
+	private float y;
+	private float z;
+
+	public Point3D(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public Point3D(Point3D p) {
+        this.x = p.x;
+        this.y = p.y;
+        this.z = p.z;
+    }
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+}
